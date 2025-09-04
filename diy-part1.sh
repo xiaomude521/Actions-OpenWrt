0be071b23dbaf6a61ab354e64be0780d06c1e49b
xiaomude521 <1780058448@qq.com>
2025-09-04 20:01:14 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+cat > feeds.conf.default << 'EOF'
+src-git packages https://git.openwrt.org/feed/packages.git
+src-git luci https://git.openwrt.org/project/luci.git
+src-git routing https://git.openwrt.org/feed/routing.git
+src-git telephony https://git.openwrt.org/feed/telephony.git
+src-git helloworld https://github.com/fw876/helloworld
+EOF
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
---
+++ diy-part2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.10.4/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+# Modify banner with dynamic version (可选)
+cat > package/base-files/files/etc/banner << 'EOF'
+_________
+    /        /\      _    ___ ___  ___
+   /  LE    /  \    | |  | __|   \| __|
+  /    DE  /    \   | |__| _|| |) | _|
+ /________/  LE  \  |____|___|___/|___|
+ \        \   DE /
+  \    LE  \    /  -------------------------------------------
+   \  DE    \  /    $(grep 'DISTRIB_DESCRIPTION' package/base-files/files/etc/openwrt_release | cut -d= -f2 | tr -d '"')
+    \________\/    -------------------------------------------
+EOF
